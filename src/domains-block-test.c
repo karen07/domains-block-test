8e03d89351b96cde7f494ce17e1b1283c75e1de1
Khachatryan Karen <karen0734@gmail.com>
2025-03-06 14:59:11 +0300

    Fix

--- src/domains-block-test.c
+++ src/domains-block-test.c
@@ -719,12 +719,12 @@ int32_t main(int32_t argc, char *argv[])
                     printf("src %s\n", src);
                 }
 
-                struct iphdr *ip_h = (struct iphdr *)(buffer + sizeof(struct ethhdr));
+                /*struct iphdr *ip_h = (struct iphdr *)(buffer + sizeof(struct ethhdr));
                 struct in_addr src_ip_s;
                 src_ip_s.s_addr = ip_h->saddr;
                 struct in_addr dst_ip_s;
                 dst_ip_s.s_addr = ip_h->daddr;
-                //printf("src dst %s %s\n", inet_ntoa(src_ip_s), inet_ntoa(dst_ip_s));
+                printf("src dst %s %s\n", inet_ntoa(src_ip_s), inet_ntoa(dst_ip_s));*/
             }
         }
     }
